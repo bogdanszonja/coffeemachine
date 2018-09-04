707de8d84295b96ab39f0d3971a298798a8a70f4
Milan Kiszely <milankiszely@gmail.com>
2018-09-04 09:49:17 +0200

    Added files and classes to make templating and network end-point to wokr

--- src/main/java/com/codecool/coffee/coffeshop/Barista.java
+++ src/main/java/com/codecool/coffee/coffeshop/Barista.java
@@ -1,4 +1,19 @@
 package com.codecool.coffee.coffeshop;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Barista {
+
+    private Deque<Order> orders = new ArrayDeque<>();
+
+    private void processOrders(){}
+
+    public void addNewOrder(Order order){
+        orders.add(order);
+    }
+
+    public void maintainCoffeMachine(){}
+
+    public void brewCoffe(){}
 }
