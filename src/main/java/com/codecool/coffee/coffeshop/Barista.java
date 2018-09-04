bb4bfcc61ac685b7c3e48fb5b5b705476cc203fb
bogdanszonja <bogdanszonja@gmail.com>
2018-09-04 13:32:22 +0200

    barista has current order and he/she can brew coffee based on that

--- src/main/java/com/codecool/coffee/coffeshop/Barista.java
+++ src/main/java/com/codecool/coffee/coffeshop/Barista.java
@@ -1,20 +1,26 @@
 package com.codecool.coffee.coffeshop;
 
+import com.codecool.coffee.sql.SQLConnection;
+import com.sun.org.apache.xpath.internal.operations.Or;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Barista {
 
+    private Order currentOrder;
+
     private Deque<Order> orders = new ArrayDeque<>();
 
-    private void processOrders(){}
+    private void preparePriorityOrder() {
+        currentOrder = SQLConnection.getPriorityOrder();
+    }
 
     public void addNewOrder(Order order){
         orders.add(order);
     }
 
     public void maintainCoffeeMachine(Trouble trouble) {
-
         if (trouble == Trouble.NOT_ENOUGH_COFFEE) {
             CoffeeMachine.getInstance().refillCoffeeBean();
         } else if (trouble == Trouble.NOT_ENOUGH_WATER) {
@@ -25,6 +31,7 @@ public class Barista {
     }
 
     public void brewCoffee() {
-
+        CoffeeMachine.getInstance().make(currentOrder.getCoffeeType());
+        SQLConnection.markOrderAsCompleted(currentOrder);
     }
 }
