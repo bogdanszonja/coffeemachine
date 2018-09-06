6b26af35dabd4596375bc6fc49342be7d22fbec1
bogdanszonja <bogdanszonja@gmail.com>
2018-09-06 10:29:46 +0200

    javadoc comments for coffeemachine

--- src/main/java/com/codecool/coffee/coffeshop/CoffeeMachine.java
+++ src/main/java/com/codecool/coffee/coffeshop/CoffeeMachine.java
@@ -3,10 +3,17 @@ package com.codecool.coffee.coffeshop;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The Coffee Machine singleton.
+ * It is capable of making coffee.
+ *
+ * @author  Szabó Anita, Kiszely Milán, Bogdán Szonja
+ * @version 1.0
+ * @since   2018-09-05
+ */
 public class CoffeeMachine {
 
     private static final Logger logger = LoggerFactory.getLogger(CoffeeMachine.class);
-
 
     private int waterLevel;
     private int coffeeBeanLevel;
@@ -17,6 +24,9 @@ public class CoffeeMachine {
 
     private static final CoffeeMachine instance = new CoffeeMachine();
 
+    /**
+     * Class constructor.
+     */
     private CoffeeMachine() {
         waterLevel = maxWaterLevel;
         coffeeBeanLevel = maxBeanLevel;
@@ -27,6 +37,13 @@ public class CoffeeMachine {
         return instance;
     }
 
+    /**
+     * Make method returns a Coffee type coffee.
+     * The class fields change based on the type of the coffee.
+     *
+     * @param coffee
+     * @return Coffee
+     */
     Coffee make(Coffee coffee) {
         logger.debug("make method called with Coffee type: {}.", coffee);
 
@@ -37,24 +54,45 @@ public class CoffeeMachine {
         return coffee;
     }
 
+    /**
+     * Set waterLevel to max.
+     */
     public void refillWater() {
         waterLevel = maxWaterLevel;
     }
 
+    /**
+     * Set coffeeBeanLevel to max.
+     */
     public void refillCoffeeBean() {
         coffeeBeanLevel = maxBeanLevel;
     }
 
+    /**
+     * Set remainingGroundLevel to max.
+     */
     public void emptyGrounds() { remainingGroundLevel = groundsCapacity; }
 
+    /**
+     *
+     * @return the current level of water in int.
+     */
     public int getWaterLevel() {
         return waterLevel;
     }
 
+    /**
+     *
+     * @return the current level of coffee beans in int.
+     */
     public int getCoffeeBeanLevel() {
         return coffeeBeanLevel;
     }
 
+    /**
+     *
+     * @return the remaining ground level in int.
+     */
     public int getRemainingGroundLevel() {
         return remainingGroundLevel;
     }
